456e6b94a3fa07eb31a58e9390ceff4b69930787
Damian Mantuano <smarbos@gmail.com>
2018-07-02 12:28:29 -0300

    [DM] Added mongoose-to-swagger and defined basic documentation information for animal service

--- src/services/animal/animal.service.js
+++ src/services/animal/animal.service.js
@@ -2,18 +2,27 @@
 const createService = require('feathers-mongoose');
 const createModel = require('../../models/animal.model');
 const hooks = require('./animal.hooks');
+const m2s = require('mongoose-to-swagger');
 
 module.exports = function (app) {
   const Model = createModel(app);
   const paginate = app.get('paginate');
 
   const options = {
-    Model,
-    paginate
+      Model,
+      paginate
+    },
+    animalService = createService(options);
+
+  animalService.docs = {
+    description: 'A service to manage animals',
+    definitions: {
+      animals: m2s(Model)
+    }
   };
 
   // Initialize our service with any options it requires
-  app.use('/animal', createService(options));
+  app.use('/animal', animalService);
 
   // Get our initialized service so that we can register hooks
   const service = app.service('animal');
